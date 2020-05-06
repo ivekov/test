9d3d6497c170871b930a6b342bd3e31c5a962efc
ivekov <i.vekov@v-halla.ru>
2020-05-06 19:31:16 -0400

    comments component template created

---
+++ local/components/ivekov/comments.list/class.php
@@ -0,0 +1,98 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Main\Application;
+use Bitrix\Main\Data\Cache;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\SystemException;
+
+class CVekovCommentsList extends CBitrixComponent
+{
+    public $requiredModules = ['iblock'];
+
+    protected $cacheDir;
+    protected $cacheID;
+
+    public function onPrepareComponentParams($params)
+    {
+        if ($params['CACHE_TYPE'] == 'Y' || $params['CACHE_TYPE'] == 'A') {
+            $params['CACHE_TIME'] = intval($params['CACHE_TIME']);
+        } else {
+            $params['CACHE_TIME'] = 0;
+        }
+
+        $params['IBLOCK_ID'] = isset($params['IBLOCK_ID']) && intval($params['IBLOCK_ID']) > 0 ? intval($params['IBLOCK_ID']) : 0;
+
+        return $params;
+    }
+
+    public function executeComponent()
+    {
+        try {
+            $this->cacheDir = '/comments_list/'.$this->arParams['ELEMENT_ID'].'/';
+            $this->cacheID = serialize([[$this->arParams['IBLOCK_ID'], $this->arParams['ELEMENT_ID']], false]);
+            $cache = Cache::createInstance();
+            if ($cache->initCache(86400, $this->cacheID, $this->cacheDir)) {
+                $vars = $cache->getVars();
+                $this->arResult = $vars['arResult'];
+            } else {
+                if ($cache->startDataCache(86400, $this->cacheID, $this->cacheDir)) {
+                    $this->checkModules();
+                    $this->prepareData();
+                    $this->buildResult();
+
+                    $cache->endDataCache([
+                        'arResult' => $this->arResult,
+                    ]);
+                }
+            }
+            $this->includeComponentTemplate();
+        } catch (Exception $e) {
+            $this->arResult['ERROR'] = $e->getMessage();
+        }
+    }
+
+    protected function prepareData()
+    {
+        $this->arResult['IBLOCK'] = [];
+        if ($this->arParams['IBLOCK_ID']) {
+            $this->arResult['IBLOCK'] = CIBlock::GetByID($this->arParams['IBLOCK_ID'])->Fetch();
+        }
+        if (!$this->arResult['IBLOCK']) {
+            throw new Exception('Инфоблок не найден');
+        }
+    }
+
+    protected function checkModules()
+    {
+        foreach ($this->requiredModules as $module) {
+            if (!Loader::includeModule($module)) {
+                throw new SystemException(Loc::getMessage('CPS_MODULE_NOT_INSTALLED', ['#NAME#' => $module]));
+            }
+        }
+    }
+
+    protected function buildResult()
+    {
+        $arParams = $this->arParams;
+
+        $arFilter = ['IBLOCK_ID' => $arParams['IBLOCK_ID'], '=PROPERTY_'.$arParams['LINK'] => $arParams['ELEMENT_ID'], 'ACTIVE' => 'Y'];
+        $arSelect = ['IBLOCK_ID', 'ID', 'NAME', 'ACTIVE', 'DETAIL_PICTURE', 'DETAIL_TEXT', 'PROPERTY_'.$arParams['LINK']];
+
+		$dbItem = CIBlockElement::GetList([], $arFilter, false, ['nPageSize' => $arParams['ELEMENTS_COUNT']], $arSelect);
+
+        $cacheManager = Application::getInstance()->getTaggedCache();
+        $cacheManager->startTagCache($this->cacheDir);
+
+        while ($element = $dbItem->Fetch()) {
+            $cacheManager->registerTag('iblock_id_'.$element['IBLOCK_ID']);
+            $this->arResult['ITEMS'][$element['ID']] = $element;
+        }
+
+        $cacheManager->endTagCache();
+    }
+}
---
+++ local/components/ivekov/comments.list/templates/.default/template.php
@@ -0,0 +1,37 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$this->setFrameMode(true);
+
+?>
+
+<section id="last-reviews" class="section section-skills post-comments post-content">
+	<div class="animate-up animated">
+		<div class="section-box" id="comments_list_wrap">
+			<ol class="comment-list" id="comments_list">
+				<? foreach($arResult['ITEMS'] as $ID => $item):?>
+				<li class="comment" id="<?=$this->GetEditAreaId($arCommentsF['ID']);?>">
+					<article class="comment-body">
+						<div class="comment-avatar"> <img src="/upload/no_photo.png" alt="avatar"> </div>
+						<div class="comment-content">
+							<div class="comment-meta"> <span class="name">Автор поста</span>
+								<time class="date" datetime="2015-03-20T13:00:14+00:00">
+									<?=$item["TIMESTAMP_X"]?>
+								</time>
+							</div>
+							<div class="comment-message">
+								<p>
+									<?=$item["DETAIL_TEXT"]?>
+								</p>
+							</div>
+						</div>
+					</article>
+				</li>
+				<?
+				endforeach;
+				?>
+				<!-- .comment -->
+			</ol>
+			<!-- .comment-list -->
+		</div>
+	</div>
+</section>
