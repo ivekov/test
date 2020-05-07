c9e5085d521c7c1e1dd477259533dceb5871e9a7
ivekov <i.vekov@v-halla.ru>
2020-05-07 20:32:01 +0000

    Apply fixes from StyleCI

--- public/list.php
+++ public/list.php
@@ -16,23 +16,23 @@ function processRequests()
 $elementId = processRequests();
 
 $APPLICATION->IncludeComponent(
-	"ivekov:comments.list", 
-	".default", 
-	array(
-		"IBLOCK_ID" => "16",
-		"COMPONENT_TEMPLATE" => ".default",
-		"IBLOCK_TYPE" => "rating",
-		"PARENT_IBLOCK_TYPE" => "DetailingCenter",
-		"PARENT_IBLOCK_ID" => "3",
-		"COMPOSITE_FRAME_MODE" => "A",
-		"COMPOSITE_FRAME_TYPE" => "AUTO",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000",
-		"ELEMENT_ID" => $elementId,
-		"LINK" => "97",
-		"ELEMENTS_COUNT" => "5"
-	),
-	false
+    'ivekov:comments.list',
+    '.default',
+    [
+        'IBLOCK_ID'            => '16',
+        'COMPONENT_TEMPLATE'   => '.default',
+        'IBLOCK_TYPE'          => 'rating',
+        'PARENT_IBLOCK_TYPE'   => 'DetailingCenter',
+        'PARENT_IBLOCK_ID'     => '3',
+        'COMPOSITE_FRAME_MODE' => 'A',
+        'COMPOSITE_FRAME_TYPE' => 'AUTO',
+        'CACHE_TYPE'           => 'A',
+        'CACHE_TIME'           => '36000',
+        'ELEMENT_ID'           => $elementId,
+        'LINK'                 => '97',
+        'ELEMENTS_COUNT'       => '5',
+    ],
+    false
 );
 ?>
 
